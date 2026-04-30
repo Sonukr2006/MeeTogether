1e2e18cc00cb27781ed46aebfcef87b2e104f89d
Sonukr2006 <sonukr96710@gmail.com>
2026-04-30 15:29:10 +0530

    add public folder i which logo and fevicon and add context folder

---
+++ client/src/contexts/AlertProvider.jsx
@@ -0,0 +1,44 @@
+import React, { createContext, useContext, useState } from "react";
+import Alert from "../UI/Alert";
+
+const AlertContext = createContext();
+
+export const useAlert = () => useContext(AlertContext);
+
+export const AlertProvider = ({ children }) => {
+  const [alerts, setAlerts] = useState([]);
+
+  const showAlert = (message, type = "error") => {
+    const id = Date.now();
+
+    setAlerts((prev) => [...prev, { id, message, type }]);
+
+    setTimeout(() => {
+      setAlerts((prev) => prev.filter((a) => a.id !== id));
+    }, 3000);
+  };
+
+  const removeAlert = (id) => {
+    setAlerts((prev) => prev.filter((a) => a.id !== id));
+  };
+
+  return (
+    <AlertContext.Provider value={{ showAlert }}>
+      
+      {children}
+
+      {/* Alert Stack */}
+      <div className="fixed top-5 right-5 space-y-3 z-50">
+        {alerts.map((alert) => (
+          <Alert
+            key={alert.id}
+            message={alert.message}
+            type={alert.type}
+            onClose={() => removeAlert(alert.id)}
+          />
+        ))}
+      </div>
+
+    </AlertContext.Provider>
+  );
+};
